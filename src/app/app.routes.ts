2cd7d26f3dff5b222f3354236461db4a5134a8f3
James Brinkerhoff <th3brink@gmail.com>
2016-07-01 12:10:59 -0600

    added chat window

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { provideRouter, RouterConfig } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component';
 import { HomeComponent } from './home/home.component';
+import { ChatComponent } from './chat/chat.component';
 
 export const routes: RouterConfig = [
   { path: '', component: HomeComponent, terminal: true },
-  { path: 'profile', component: ProfileComponent }
+  { path: 'profile', component: ProfileComponent },
+  { path: 'chat', component: ChatComponent },
 ];
 
 export const APP_ROUTER_PROVIDERS = [
